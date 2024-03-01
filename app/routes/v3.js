78cd03577f2f08804d5258c0a7dfd65370c343f8
MhariEliz <mhari.mcnaught@engineering.digital.dwp.gov.uk>
2024-03-01 12:05:41 +0000

    draft v3

---
+++ app/routes/v3.js
@@ -0,0 +1,12 @@
+module.exports = function (router) {
+
+// GENERIC NEXT PAGE ELEMENT
+router.post('*', function (req, res, next) {
+    console.log(req.body);
+    if (req.body['next-page']) {
+      res.redirect(req.body['next-page']);
+    } else {
+      next();
+    }
+  });
+}
