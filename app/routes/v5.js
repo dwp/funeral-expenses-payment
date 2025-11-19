81a8abe2ff574e6227f8e5776c70669f2a408fe3
Jonathan Branthwaite <jonathan.branthwaite@engineering.digital.dwp.gov.uk>
2025-11-19 09:19:30 +0000

    feat: second attempt at removing routers
    GitOrigin-RevId: 795efcbaefdbd66f06ef3c94b4033a69b0c37ec7

--- app/routes/v5.js
+++ app/routes/v5.js
@@ -1,15 +1,6 @@
 module.exports = function (router) {
 
 
-// GENERIC NEXT PAGE ELEMENT
-router.post('*', function (req, res, next) {
-    console.log(req.body);
-    if (req.body['next-page']) {
-      res.redirect(req.body['next-page']);
-    } else {
-      next();
-    }
-  });
 
 // ARE YOU THE PARTNER
 
