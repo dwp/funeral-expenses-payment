68ab31b472653748113d27b6f824501753445b20
Jonathan Branthwaite <jonathan.branthwaite@engineering.digital.dwp.gov.uk>
2025-11-19 09:15:53 +0000

    feat: updated routes to push
    GitOrigin-RevId: 4d4196e5111d167c4f8162f454e2a8a110fdc297

--- app/routes/v1.js
+++ app/routes/v1.js
@@ -1,5 +1,14 @@
 module.exports = function (router) {
 
+// GENERIC NEXT PAGE ELEMENT
+router.post('*', function (req, res, next) {
+    console.log(req.body);
+    if (req.body['next-page']) {
+      res.redirect(req.body['next-page']);
+    } else {
+      next();
+    }
+  });
   
     // TEST ROUTE RELATIONSHIP INELIGBLE JOURNEY
 
--- app/routes/v6.js
+++ app/routes/v6.js
@@ -1,6 +1,15 @@
 module.exports = function (router) {
 
 
+// GENERIC NEXT PAGE ELEMENT
+router.post('*', function (req, res, next) {
+    console.log(req.body);
+    if (req.body['next-page']) {
+      res.redirect(req.body['next-page']);
+    } else {
+      next();
+    }
+  });
 
 // RESPONSIBLE FOR PAYING
 
